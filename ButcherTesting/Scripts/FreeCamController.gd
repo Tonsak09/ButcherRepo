8d532a71e2662a9f16cf462b8259ca0065998c26
Narai Risser (RIT Student) <43816475+Tonsak09@users.noreply.github.com>
2024-08-03 14:52:20 -0400

    Working!
    - Colored ascii art
    - free cam

---
+++ ButcherTesting/Scripts/FreeCamController.gd
@@ -0,0 +1,51 @@
+extends Node3D
+
+@export var speed : float
+
+@export var speedH = 2.0
+@export var speedV = 2.0
+
+var holdMouse : Vector2
+var yaw = 0.0
+var pitch = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	holdMouse = get_viewport().get_mouse_position()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	MovementBody(delta)
+	#MovementRot(delta)
+	
+	if Input.is_action_just_pressed("Quit"):
+		get_tree().quit()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		# modify accumulated mouse rotation
+		yaw += event.relative.x * -speedH * get_process_delta_time()
+		pitch += event.relative.y * -speedV * get_process_delta_time()
+		transform.basis = Basis() # reset rotation
+		rotate_object_local(Vector3(0, 1, 0), yaw) # first rotate in Y
+		rotate_object_local(Vector3(1, 0, 0), pitch) # then rotate in X
+
+
+func MovementBody(delta):
+	var moveAxis : Vector3
+	
+	if Input.is_action_pressed("Forward"):
+		moveAxis -= transform.basis.z
+	elif Input.is_action_pressed("Backward"):
+		moveAxis += transform.basis.z
+	
+	if Input.is_action_pressed("Right"):
+		moveAxis += transform.basis.x
+	elif Input.is_action_pressed("Left"):
+		moveAxis -= transform.basis.x
+	
+	moveAxis = moveAxis.normalized()
+	position += moveAxis * speed * delta;
