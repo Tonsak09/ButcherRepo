f65de3764cb616356b2fe00b550ed4357fe6be06
Narai Risser <43816475+Tonsak09@users.noreply.github.com>
2024-08-11 18:12:37 -0400

    Link to start scene from console

--- ButcherTesting/Scripts/IntroScene/Console.gd
+++ ButcherTesting/Scripts/IntroScene/Console.gd
@@ -1,33 +1,75 @@
-extends MeshInstance3D
+extends Node3D
 
 @export var terminalLabel : MeshInstance3D
 @export var screen : TerminalScreens
 
+# Startup screen 
+@export var startupTime : float 
 @export var dotTimer : float 
 @export var lines : Array[String]
+
+# Code display 
+@export var codeTime : float 
 @export var codePause : float 
 @export var codeActiveMax : int 
 @export var codeLines : Array[String]
 
+# Terminal input 
+@export var maxNameSize : int 
+@export var underscoreTime : float 
+
 var currDot : int
 var currCode : int 
 
+var stateTimer : float 
 var timer : float 
+
+var inputName : String
+var showUnderscore : bool 
+var checkIfConfirm : bool 
+
+var introScene
 
 enum TerminalScreens { STARTUP, CODE, INTERFACE }
 
 func _ready():
 	currDot = 0
+	introScene = load("res://Scenes/MainScene.tscn")
 
 func _process(delta):
 	
 	match screen:
 		TerminalScreens.STARTUP:
 			Startup(delta)
+			stateTimer += delta
+			if stateTimer >= startupTime:
+				stateTimer = 0.0
+				screen = TerminalScreens.CODE
 		TerminalScreens.CODE:
 			Code(delta)
+			stateTimer += delta
+			if stateTimer >= codeTime:
+				stateTimer = 0.0
+				screen = TerminalScreens.INTERFACE
+		TerminalScreens.INTERFACE:
+			Interface(delta)
+
+func _input(event):
 	
 	
+	if screen != TerminalScreens.INTERFACE:
+		return
+	
+	if event is InputEventKey && event.is_pressed():
+		var input : String 
+		input = event.as_text()
+		
+		match input:
+			"Backspace":
+				inputName = inputName.left(inputName.length() - 1)
+			_:
+				if input.length() == 1 and inputName.length() < maxNameSize:
+					inputName += input
 
 # Bootup stage 
 func Startup(delta):
@@ -53,16 +95,18 @@ func GetDot(count : int):
 	
 	return dotText 
 
+# Runs code across screen 
 func Code(delta):
+	
+	if currCode > codeLines.size():
+		return
+	
 	terminalLabel.mesh.text = GetCode(currCode)
 	
 	timer += delta
 	if timer >= codePause:
 		timer = 0.0
-		
 		currCode += 1
-		if currCode > codeLines.size():
-			currCode = 0
 
 func GetCode(codeLine : int):
 	var minValue = max(0, codeLine - codeActiveMax) 
@@ -73,3 +117,27 @@ func GetCode(codeLine : int):
 		codeStr += codeLines[minValue + i] + "\n"
 	
 	return codeStr
+
+# Indicates player can type name and handles the input 
+func Interface(delta):
+	
+	if checkIfConfirm :
+		if Input.is_action_pressed("ui_accept"):
+			var instance = introScene.instantiate()
+			get_tree().root.add_child(instance)
+			get_node("/root/ConsoleInteract").free()
+		return 
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			checkIfConfirm = true
+
+	
+	terminalLabel.mesh.text = "Name ID:\n" + inputName 
+	
+	if showUnderscore && inputName.length() < maxNameSize:
+		terminalLabel.mesh.text += "_"
+	
+	timer += delta; 
+	if timer >= underscoreTime:
+		timer = 0.0
+		showUnderscore = !showUnderscore
